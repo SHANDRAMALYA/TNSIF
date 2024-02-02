dc81b176dac0f3ebc3b1df94f3e3839f419a697c
SHANDRAMALYA <132643403+SHANDRAMALYA@users.noreply.github.com>
2024-02-02 01:23:03 -0600

    Add files via upload

---
+++ ASSIGNMENT_6/squareFill.java
@@ -0,0 +1,32 @@
+package ASSIGNMENT_6;
+//Java Program to print
+//Square fill pattern
+import java.util.*;
+
+public class squareFill {
+
+ // Function to demonstrate pattern
+ public static void printPattern(int n)
+ {
+     int i, j;
+
+     // outer loop to handle rows
+     for (i = 0; i <= n; i++) {
+
+         // inner loop to handle columns
+         for (j = 0; j <= n; j++) {
+             System.out.print("*");
+         }
+
+         // printing new line for each row
+         System.out.println();
+     }
+ }
+
+ // Driver Function
+ public static void main(String args[])
+ {
+     int n = 6;
+     printPattern(n);
+ }
+}
